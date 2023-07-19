75d940aac4f384fe199e2cddb8f4ae2fe5a72b52
gagan4580 <51686267+gagan4580@users.noreply.github.com>
2023-07-19 17:45:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Second Commit
+          First Commit
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I am Genius !!!!!
+          I am a Genius
         </a>
       </header>
     </div>
